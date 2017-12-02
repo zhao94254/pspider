9706aab0f5be6b118f24c65f495a96365ee64801
zhao94254 <zhao942548838@qq.com>
2017-12-02 10:37:28 +0800

    update

--- danmu/a_data.py
+++ danmu/a_data.py
@@ -13,10 +13,19 @@ db = client.crawl
 danmu = db.danmu
 
 
-def get_sort():
-    d = danmu.aggregate([{"$group": {"_id": "$rid", "count": {"$sum": 1}}}, {"$sort": SON([("count", -1)])}])
-    for i in d:
-        print(i)
+def get_sort(s):
+    """根据不同的字段，按照不同类型统计排行
+    主要是rid 主播房间 uid 用户 level 等
+    """
+    d = danmu.aggregate([{"$group": {"_id": "${}".format(s), "count": {"$sum": 1}}}, {"$sort": SON([("count", -1)])}])
+    for i, j in enumerate(d):
+        if i == 50:
+            break
+        print(j)
 
-
-get_sort()
+if __name__ == '__main__':
+    get_sort('rid')
+    print('-' * 50)
+    get_sort('uid')
+    print('-' * 50)
+    get_sort('level')
