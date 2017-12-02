51702c1f5ac18467f45e0a301ffcc672dd69e3cf
zhao94254 <zhao942548838@qq.com>
2017-12-02 11:12:40 +0800

    fix bug

--- danmu/a_data.py
+++ danmu/a_data.py
@@ -17,15 +17,18 @@ def get_sort(s):
     """根据不同的字段，按照不同类型统计排行
     主要是rid 主播房间 uid 用户 level 等
     """
-    d = danmu.aggregate([{"$group": {"_id": "${}".format(s), "count": {"$sum": 1}}}, {"$sort": SON([("count", -1)])}])
+    d = danmu.aggregate([{"$group": {"_id": "${}".format(s), "count": {"$sum": 1}}},
+                         {"$sort": SON([("count", -1)])}], allowDiskUse=True) # allowDiskUse 启动这个，防止内存不够用
     for i, j in enumerate(d):
         if i == 50:
             break
         print(j)
+    print('end')
 
 if __name__ == '__main__':
+    print('房间排名','-' * 50)
     get_sort('rid')
-    print('-' * 50)
+    print('用户','-' * 50)
     get_sort('uid')
-    print('-' * 50)
+    print('等级','-' * 50)
     get_sort('level')
