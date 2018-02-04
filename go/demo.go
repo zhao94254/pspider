222faf759830dc793ca82f1538173617236ecbcf
zhao94254 <zhao942548838@qq.com>
2018-02-05 07:42:30 +0800

    增加go版本

---
+++ go/demo.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+"fmt"
+"net"
+"encoding/binary"
+"bytes"
+"errors"
+"strings"
+)
+
+const (
+	BufferSize  = 1024
+	ServerAddr  = "openbarrage.douyutv.com:8601"
+	//ServerAddr = "localhost:9999"
+	PostCode = 689
+	PullCode = 690
+	Roomid = 688
+)
+
+func PostData(msg string) []byte {
+	// 构造需要发送的二进制数据
+	length := 9+len(msg) // 长度4字节 + 类型2字节 + 加密字段1字节 + 保留字段1字节 + 结尾字段1字节
+	buffer := bytes.NewBuffer([]byte{})
+	binary.Write(buffer, binary.LittleEndian, int32(length))
+	binary.Write(buffer, binary.LittleEndian, int32(length))
+	binary.Write(buffer, binary.LittleEndian, int16(PostCode))
+	binary.Write(buffer, binary.LittleEndian, int8( 0))
+	binary.Write(buffer, binary.LittleEndian, int8(0))
+	binary.Write(buffer, binary.LittleEndian, []byte(msg))
+	binary.Write(buffer, binary.LittleEndian, int8(0))
+	fmt.Println(buffer)
+	return buffer.Bytes()
+}
+
+func JoinRoom(roomid string)[]byte  {
+	// 选择要链接的房间号
+	msg := fmt.Sprintf("type@=loginreq/roomid@=%s/", roomid)
+	return PostData(msg)
+}
+
+func JoinMsg(roomid string)[]byte{
+	msg := fmt.Sprintf("type@=joingroup/rid@=%s/gid@=-9999/", roomid)
+	return PostData(msg)
+}
+
+func ParseData(buffer []byte) map[string]interface{} {
+	// 解析， 将二进制数据转化为可读的
+	Parsed := make(map[string]interface{})
+	s := strings.Trim(string(buffer), "/")
+	items := strings.Split(s, "/")
+	for _, str := range items {
+		k := strings.SplitN(str, "@=", 2)
+		fmt.Println(len(k))
+		Parsed[k[0]] = k[1]
+
+	}
+	return Parsed
+}
+
+func TestConn()  {
+	buffer := make([]byte, BufferSize)
+	JoinData := JoinRoom("688")
+	JoinMsg := JoinMsg("688")
+	conn, _ := net.Dial("tcp", ServerAddr)
+	fmt.Println(JoinData)
+	_, werr := conn.Write(JoinData)
+	if werr != nil{
+		fmt.Println(werr)
+	}
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println(errors.New("无法连接房间 " + err.Error()))
+	}
+	conn.Write(JoinMsg)
+	for  {
+		_, err := conn.Read(buffer)
+		parsed := ParseData(buffer)
+		fmt.Println(parsed)
+		//fmt.Println(parsed["level"],parsed["nn"], parsed["txt"])
+		if err != nil{
+			break
+		}
+	}
+
+
+
+
+	//conn.Close()
+	fmt.Println(string(buffer))
+}
+
+func Connect()  {
+	conn, _ := net.Dial("tcp", "localhost:9999")
+	buffer := make([]byte, BufferSize)
+	//wbuff := new(bytes.Buffer)
+	//binary.Write(wbuff, binary.LittleEndian, []byte("test"))
+	wbuff := []byte("3030type@=loginreq/roomid@=688/")
+	fmt.Println(wbuff)
+	conn.Write(wbuff)
+	conn.Read(buffer)
+	fmt.Println(string(buffer))
+}
+
+func main()  {
+	//Connect()
+	//fmt.Println(PostData("test"))
+	TestConn()
+	//JoinRoom("688")
+}
+
+
