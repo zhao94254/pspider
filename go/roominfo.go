a860210bbf3259f6123623d434b9e26a3dc41432
zhao94254 <zhao942548838@qq.com>
2018-02-06 00:02:02 +0800

    频道和在线房间

--- go/roominfo.go
+++ go/roominfo.go
@@ -5,18 +5,32 @@ import (
 	"fmt"
 	"io/ioutil"
 	"encoding/json"
+	"strconv"
 )
 
-func main()  {
+func Pindao()  {
 	var dat map[string]interface{}
 	resp, _ := http.Get("http://open.douyucdn.cn/api/RoomApi/game")
 	b, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(b, &dat)
-	//fmt.Println(dat["data"]) // 获取频道
-	for _, j :=  range dat{
-		fmt.Println(j)
-		fmt.Println("\n")
+	mapTmp := dat["data"].([]interface{})
+	for _, j :=  range mapTmp{
+		l := j.(map[string]interface{})
+		fmt.Println(l["game_name"], l["cate_id"]) // 获得频道id
+		Liveid(l["cate_id"].(float64))
 	}
+}
 
+func Liveid(pindao float64)  {
+	p := strconv.FormatFloat(pindao,'f',0,64)
+	resp, _ := http.Get(fmt.Sprintf("http://api.douyu.com/api/v1/live/%s", p))
+	// todo parse roomid
+	b, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(b))
+}
+
+
+func main()  {
+	Pindao()
 }
 
