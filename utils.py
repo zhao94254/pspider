924b709b93172a5d410849a6b8b550fb7afff180
zhaopengyun <zhao942548838@qq.com>
2019-03-11 17:31:53 +0800

    get local ip

--- utils.py
+++ utils.py
@@ -3,3 +3,15 @@
 # @Created on    : 2019-03-06 17:18
 # @Author  : zpy
 # @Software: PyCharm
+
+def get_local_ip():
+    import socket
+    res = ''
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        res = s.getsockname()[0]
+        s.close()
+    except Exception:
+        pass
+    return res
