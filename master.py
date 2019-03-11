7442c094fa5b8f2d5896969b7b37b0cda0c64ac9
zhaopengyun <zhao942548838@qq.com>
2019-03-11 16:53:04 +0800

    添加寿命

--- master.py
+++ master.py
@@ -3,3 +3,5 @@
 # @Created on    : 2019-03-11 11:58
 # @Author  : zpy
 # @Software: PyCharm
+
+# 分发任务，控制slave
--- slave.py
+++ slave.py
@@ -3,3 +3,5 @@
 # @Created on    : 2019-03-11 11:58
 # @Author  : zpy
 # @Software: PyCharm
+
+# 用来接收master分发的任务执行
