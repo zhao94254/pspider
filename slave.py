6a556e95ee831e2ae9cf9417613ee4dc6dffac8a
zhaopengyun <zhao942548838@qq.com>
2019-03-11 17:30:26 +0800

    add abs

--- slave.py
+++ slave.py
@@ -5,3 +5,25 @@
 # @Software: PyCharm
 
 # 用来接收master分发的任务执行
+
+
+def register_slave():
+    """
+    将 slave 注册到系统
+    :return:
+    """
+    pass
+
+def start_task():
+    """
+    启动一批任务的进程
+    :return:
+    """
+    pass
+
+def stop_task():
+    """
+    停止一批任务的进程
+    :return:
+    """
+    pass
