801085635414dbc8a9975c28ea4fba06b1fc3ad2
zhaopengyun <zhao942548838@qq.com>
2019-03-11 22:41:29 +0800

    添加抽象

---
+++ app/task.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 2019-03-11 22:25
+# @Author  : zpy
+# @Software: PyCharm
+
+
+class Task(object):
+    """
+    在 celery 上封装一层， 任务的调度，执行，分发都会依靠这里来做
+    """
+
+    app = None
+
+    def __init__(self):
+        pass
+
+    def set_config(self):
+        pass
+
+    @classmethod
+    def clstasks(cls, **kwargs):
+        return cls.app.task(bind=True, **kwargs)
+
+    @classmethod
+    def ptask(cls, **kwargs):
+        @cls.clstasks(**kwargs)
+        def _instance(self, **kwargs):
+            cls(**kwargs).start()
+        return _instance
+
+    def start(self):
+        pass
+
+    def execute(self):
+        pass
