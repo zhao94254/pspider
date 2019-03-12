e7a116c96e7ad9d2eecac224c5ef85a5c1c11852
zhaopengyun <zhao942548838@qq.com>
2019-03-12 16:06:12 +0800

    add celery conf

--- app/celery.py
+++ app/celery.py
@@ -3,3 +3,36 @@
 # @Created on    : 2019-03-12 15:42
 # @Author  : zpy
 # @Software: PyCharm
+
+from celery import Celery
+from kombu import Queue, Exchange
+from conf.config import celery_broker
+from plogger import get_logger
+
+
+log = get_logger('celery_init')
+
+capp = Celery(
+    'app',
+    broker=celery_broker
+)
+
+# 保证任务是可靠的执行了
+capp.conf.update(CELERY_REJECT_ON_WORKER_LOST=True, CELERY_ACKS_LATE=True)
+
+def init_sdks():
+    from app.sdks import _all_sdk_
+    from app import tasks
+    queues = []
+    for s in _all_sdk_:
+        s.app = capp
+        name = str(s)
+        tasks.__dict__[name] =  s.ptask()
+        queues.append(Queue(name, exchange=Exchange(name, type='direct'), routing_key=name))
+
+    capp.conf.update(
+        CELERY_QUEUES=queues
+    )
+    capp.conf.update(CELERY_INCLUDE=['app.tasks'])
+
+init_sdks()
