ea59c279d29c28121a22ff6007356f08fc193fcf
zhaopengyun <zhao942548838@qq.com>
2019-03-14 10:55:46 +0800

    addslave

--- master.py
+++ master.py
@@ -6,9 +6,23 @@
 
 # 分发任务，控制slave
 
+from flask import Flask, request
+from conf.config import redis_client
+
+app = Flask(__name__)
+
+@app.route('/addslave')
 def add_slave():
-    pass
+    redis_client.sadd('master|allslaves', request.remote_addr)
+    return 'ok'
 
+@app.route('/allslaves')
 def all_slaves():
-    pass
+    res = redis_client.smembers('master|allslaves')
+    slaves = list(map(lambda x:x.split('|')[1], res))
+    slaves.sort()
+    return slaves
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
