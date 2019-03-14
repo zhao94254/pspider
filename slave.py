8c4e3184a31d9b0ac84a266f9ebc211b884dd9a2
zhaopengyun <zhao942548838@qq.com>
2019-03-14 14:29:12 +0800

    add slave

--- slave.py
+++ slave.py
@@ -5,16 +5,25 @@
 # @Software: PyCharm
 
 # 用来接收master分发的任务执行
+# python3.7 安装supervisor
+# pip install git+https://github.com/Supervisor/supervisor.git
+#
 
 # todo 完善基本功能
 # todo 添加docker
+from flask import Flask, request
+import xmlrpc.client
+import requests
+
+server = xmlrpc.client.ServerProxy('http://127.0.0.1:9001/RPC2')
+
 
 def register_slave():
     """
     将 slave 注册到系统
     :return:
     """
-    pass
+    requests.get('master..')
 
 def start_task():
     """
