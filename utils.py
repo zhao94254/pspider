ca3d8884c7017d64ee7d6d6fa6f4c29fa4ebb153
zhaopengyun <zhao942548838@qq.com>
2019-03-18 17:02:47 +0800

    mysql context

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@
 # @Author  : zpy
 # @Software: PyCharm
 
+import pymysql
+from pymysql import cursors
+
 def get_local_ip():
     import socket
     res = ''
@@ -15,3 +18,20 @@ def get_local_ip():
     except Exception:
         pass
     return res
+
+class Mysql:
+    def __init__(self, stream=False, **kw): #  MySQLdb.cursors.SSCursor 流式游标
+        if stream == True:
+            self._connect = pymysql.connect(cursorclass = cursors.SSCursor, **kw)
+        else:
+            self._connect = pymysql.connect(**kw)
+    def __enter__(self):
+
+        self.cursor = self._connect.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+
+        self._connect.commit()
+        self.cursor.close()
+        self._connect.close()
