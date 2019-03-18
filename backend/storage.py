889aab064986fb122dc35682171ad2a9045a9004
zhaopengyun <zhao942548838@qq.com>
2019-03-18 17:13:38 +0800

    add abs

--- backend/storage.py
+++ backend/storage.py
@@ -16,9 +16,36 @@ class BaseBackend(object):
     def __init__(self, app):
         self.app = app
 
+    def pre_check(self):
+        """
+        检查传入的配置、参数
+        :return:
+        """
+        pass
+
 
 class MongoBackend(BaseBackend):
 
-    def save(self, data):
+    def save(self, data): # todo 这里应该传入数据还是？
         log.info("{} {} insert {}".format(str(self.app), self.app.group, len(data)))
         mongo_storage[str(self.app)][self.app.group].insert_many(data)
+
+
+class MysqlBackend(BaseBackend):
+
+    def save(self, data, sql):
+        pass
+
+
+class RedisBackend(BaseBackend):
+
+    def save(self, data):
+        pass
+
+def dispatch(conf):
+    """
+    根据配置指到不同的类
+    :param conf:
+    :return:
+    """
+    pass
